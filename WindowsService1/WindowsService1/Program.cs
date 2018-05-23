9bfb7e92c75ce15b26dcb1367c09ff5819fc130d
Daniel <34353662+DanielArevalo@users.noreply.github.com>
2018-05-23 13:33:31 -0500

    service

---
+++ WindowsService1/WindowsService1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    static class Program
+    {
+        private static readonly globaldeEntities Db = new globaldeEntities();
+        static void Main()
+        {
+
+#if DEBUG
+            var svc = new Service1();
+            svc.OnNewExecution();
+            Thread.Sleep(Timeout.Infinite);
+#else
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+
+
+    }
+}
